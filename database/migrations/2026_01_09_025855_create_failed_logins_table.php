3060df0337a8f2c01ca391595d26e9bd62781763
itsZekiee <habee2004@gmail.com>
2026-01-09 04:23:59 +0800

    fix: add missing

---
+++ database/migrations/2026_01_09_025855_create_failed_logins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('failed_logins', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->string('reason')->nullable();
+            $table->timestamp('created_at')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('failed_logins');
+    }
+};
